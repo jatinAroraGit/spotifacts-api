6bb5233114860ceeead0e6d8a62bce783d6be3b6
Jatin Arora <jaarora45@gmaill.com>
2021-12-20 21:28:29 +0530

    Finished adding routes for gettin access token and fallback route for refreshing token

--- index.js
+++ index.js
@@ -68,7 +68,17 @@ app.get('/callback', (req, res) => {
       if (response.status === 200) {
 
         const { access_token, token_type } = response.data;
-
+        axios.get('https://api.spotify.com/v1/me', {
+          headers: {
+            Authorization: `${token_type} ${access_token}`
+          }
+        })
+          .then(response => {
+            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+          })
+          .catch(error => {
+            res.send(error);
+          });
       } else {
         res.send(response);
       }
@@ -77,6 +87,27 @@ app.get('/callback', (req, res) => {
       res.send(error);
     });
 
+});
+
+app.get('/refresh_token', (req, res) => {
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+    },
+  })
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(error => {
+      res.send(error);
+    });
 });
 
 app.listen(port, () => {
